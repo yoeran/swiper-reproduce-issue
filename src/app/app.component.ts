de0b6e59121eb4ca5b84bd2700071243d7c27493
Yoeran Luteijn <yoeran.luteijn@aegon.nl>
2021-03-15 11:25:46 +0100

    Reproduce bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import SwiperCore, { A11y, Pagination } from 'swiper/core';
+
+SwiperCore.use([Pagination, A11y]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'SwiperBugReproduce';
+
+  onSwiper(swiper) {
+    console.log('onswiper');
+  }
 }
